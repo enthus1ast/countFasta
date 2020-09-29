1bfae5d5c625e8ba16f0fda3c47f8b540e07fc04
David Krause <krause@biochem2.uni-frankfurt.de>
2020-09-29 20:12:44 +0200

    Signed-off-by: David Krause <krause@biochem2.uni-frankfurt.de>

---
+++ README.md
@@ -0,0 +1,38 @@
+High performance tool to (correctly) count sequences in a fasta file.
+
+# How to get it:
+
+  ## build it yourself
+  with Nim ( https://nim-lang.org ):
+
+  ```
+    nim c -d:release --opt:speed -d:danger -r "fastalib.nim"
+  ```
+
+  ## download prebuild binary
+
+  - [](windows)
+
+# usage:
+
+  Count sequences in fasta files:
+
+  ```
+    fastalib.exe count -p *.fasta
+    92169   tt.fasta
+    737352  tt2.fasta
+  ```
+
+  Count sequences in fasta file, but also report sequences where an additional ">" is in the description.
+  These lines give wrong result when just counting ">" in an editor or with grep.
+
+  ```
+    fastalib.exe report -p *.fasta
+    [wrong] line:365        pos:22884       string:>sp|P14060|3BHS1_HUMAN 3 beta-hydroxysteroid dehydrogenase/Delta 5-->4-isomerase type 1 OS=Homo sapiens GN=HSD3B1 PE=1 SV=2
+    [wrong] line:638        pos:39741       string:>sp|P31941|ABC3A_HUMAN DNA dC->dU-editing enzyme APOBEC-3A OS=Homo sapiens GN=APOBEC3A PE=1 SV=3
+    [wrong] line:643        pos:40046       string:>sp|P31941-2|ABC3A_HUMAN Isoform 2 of DNA dC->dU-editing enzyme APOBEC-3A OS=Homo sapiens GN=APOBEC3A
+    [wrong] line:648        pos:40334       string:>sp|Q8IUX4|ABC3F_HUMAN DNA dC->dU-editing enzyme APOBEC-3F OS=Homo sapiens GN=APOBEC3F PE=1 SV=3
+    [wrong] line:656        pos:40816       string:>sp|Q8IUX4-2|ABC3F_HUMAN Isoform 2 of DNA dC->dU-editing enzyme APOBEC-3F OS=Homo sapiens GN=APOBEC3F
+    .....
+    737352  tt2.fasta
+  ```
---
+++ fastalib.nim
@@ -0,0 +1,81 @@
+# compile with:
+# nim c -d:release --opt:speed -d:danger -r "more.nim"
+import memfiles
+
+template `[]`(mem: pointer, pos: uint): char =
+  cast[ptr char](cast[ByteAddress](mem) + pos.ByteAddress)[]
+
+proc cnt*(fhm: Memfile): uint =
+  ## fastest way (i found) to count sequences in a fasta file
+  result = 0
+  var lastWasNl = true
+  var pos: uint = 0
+  while true:
+    if pos > fhm.size.uint: break
+    case fhm.mem[pos]
+    of '\n':
+      lastWasNl = true
+    of '>':
+      if lastWasNl: result.inc
+    else:
+      lastWasNl = false
+    pos.inc
+
+proc toString(fhm: MemFile, startPos, endPos: uint): string =
+  result = ""
+  for idx in startPos..endPos:
+    result.add fhm.mem[idx]
+
+proc cntReport*(fhm: MemFile): uint =
+  ## Reports sequences that contains a '>' somewhere in description.
+  result = 0
+  var lastWasNl = true
+  var pos: uint = 0
+  var line = 0
+  var start: uint = 0
+  var isWrong = false
+  while true:
+    if pos > fhm.size.uint: break
+    case cast[ptr char](cast[ByteAddress](fhm.mem) + pos.ByteAddress)[]
+    of '\n':
+      if isWrong:
+        echo "[wrong] line:" , line, "\tpos:", pos , "\tstring:", fhm.toString(start, pos-1)
+        isWrong = false
+      lastWasNl = true
+      start = pos + 1
+      line.inc
+    of '>':
+      if lastWasNl: result.inc
+      else:
+        isWrong = true
+    else:
+      lastWasNl = false
+    pos.inc
+
+proc cnt*(path: string): uint =
+  var memfile = memfiles.open(path)
+  result = memfile.cnt()
+  memfile.close()
+
+when isMainModule:
+  import cligen
+  import os
+
+  proc count(paths: string) =
+    ## Counts sequences in fasta files:
+    ## Usage:
+    ##  cntfasta mycnt -p myFastaFile.fasta
+    ##  cntfasta mycnt -p *.fasta
+    for path in walkPattern(paths):
+      var memfile = memfiles.open(path)
+      echo cnt(memfile) , "\t", path
+      memfiles.close(memfile)
+
+  proc report(paths: string) =
+    ## Reports sequences that contains a '>' somewhere in description.
+    for path in walkPattern(paths):
+      var memfile = memfiles.open(path)
+      echo cntReport(memfile) , "\t", path
+      memfiles.close(memfile)
+
+  dispatchMulti([count], [report])
